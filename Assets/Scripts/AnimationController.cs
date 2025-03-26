47ed6b61eaf79bb082785e99e07d4e148dc2bb39
Nayan <shubhamtekam7083@gmail.com>
2025-03-26 14:19:00 +0530

    applied crouch animation and Jump animation

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] BoxCollider2D boxCollider;
+    private Vector2 boxcolSize;
+    private Vector2 boxcolOffset;
+
+    private void Start()
+    {
+        boxcolSize= boxCollider.size;
+        boxcolOffset= boxCollider.offset;
+    }
 
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
         Vector2 scale= transform.localScale;
-        animator.SetFloat("Speed",Mathf.Abs( speed));
+        animator.SetFloat("Speed",Mathf.Abs(speed));
         
         if(speed < 0)
         {
@@ -19,6 +29,49 @@ public class AnimationController : MonoBehaviour
         {
             scale.x=Mathf.Abs(speed);
         }
-        transform.localScale = scale;   
+        transform.localScale = scale;
+
+        PlayJumpAnimation(verticalInput);
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            PlayCrouchAnimation(true);
+        }
+        else
+        {
+            PlayCrouchAnimation(false);
+        }
+            
+    }
+
+
+    private void PlayJumpAnimation(float verticalInput)
+    {
+        if (verticalInput > 0)
+        {
+            animator.SetTrigger("Jump");
+        }
+       
+    }
+
+    private void PlayCrouchAnimation(bool crouch)
+    {
+       if(crouch== true)
+        {
+            float sizeX = 0.483f;
+            float sizeY = 1.173f;
+            float OffX = -0.003f;
+            float OffY = 0.553f;
+
+            boxCollider.offset=new Vector2(OffX, OffY);
+            boxCollider.size= new Vector2(sizeX, sizeY);
+
+        }
+        else
+        {
+            boxCollider.offset =boxcolOffset;
+            boxCollider.size = boxcolSize;
+        }
+            animator.SetBool("Crouch", crouch);
     }
 }
